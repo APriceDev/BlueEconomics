4a6d84567038affa055a0b58f0693dcb985e3b35
apricedev <alanprice.dev1@gmail.com>
2013-08-28 12:13:45 -0400

    updating faq's

---
+++ src/BlueEconomics.Platform/Domain/FAQ_QuestionAssignment.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace BlueEconomics.Platform.Domain 
+{
+    public class FAQ_QuestionAssignment
+    {
+        public FAQ_QuestionAssignment()
+        {
+            IsAnswered = false;
+            dateAssigned = DateTime.Now;
+            dateAnswered = DateTime.MaxValue;
+        }
+
+        [Key]
+        public int Id { get; set; }
+ 
+        [Required]
+        public DateTime dateAssigned { get; set; }
+        public DateTime dateAnswered { get; set; }
+
+        public bool IsAnswered { get; set; }
+
+        // Foreign key into FAQ_ResponseSource
+        public int FAQ_ResponseSourceId { get; set; }
+        public FAQ_ResponseSource FAQ_ResponseSource { get; set; }
+
+        // Foreign key into FAQ_Question table
+        public int FAQ_QuestionId { get; set; }
+        public FAQ_Question FAQ_Question { get; set; }
+    }
+}
